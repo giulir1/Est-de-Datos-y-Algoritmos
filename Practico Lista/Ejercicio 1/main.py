0e04cc08a9363d9ed8e7d8e12a7a9cd3bd342245
giulir1 <83610835+giulir1@users.noreply.github.com>
2022-09-06 13:37:28 -0300

    Add files via upload

--- Practico Lista/Ejercicio 1/main.py
+++ Practico Lista/Ejercicio 1/main.py
@@ -1,10 +1,10 @@
-from listaSecuencial import ListaSecuencial
+from listaSecuencialContenido import ListaSecuencialCont
 
 if __name__ == '__main__':
-    lista = ListaSecuencial(5)
+    lista = ListaSecuencialCont(4)
     lista.crear()
-    lista.insertar(0, 1)
-    lista.insertar(1, 2)
-    lista.insertar(2, 3)
-    lista.insertar(1, 55)
-    lista.suprimir(1)
+    lista.insertar(1)
+    lista.insertar(4)
+    lista.insertar(3)
+    lista.insertar(2)
+    lista.suprimir(0)
